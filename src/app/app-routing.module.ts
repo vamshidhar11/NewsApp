9a48f08b6162c1d831508563eca0bf924ed545d0
vamshidhar <vamshidhar.reddy@insightorb.com>
2020-02-03 20:06:06 +0530

    added lazy loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes =
-[
-  {path: '', redirectTo: '/news-center', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
+const routes: Routes = [
+  { path: '', redirectTo: 'news-center', pathMatch: 'full' },
+  {
+    path: 'news-center',
+    loadChildren: () =>
+      import('./news-center/news-center.module').then(m => m.NewsCenterModule)
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
